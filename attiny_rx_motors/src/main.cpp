d700ea3e3c609f96116fe4e5b0061fb5040c9f11
Bryan Boettcher <bryan.boettcher@gmail.com>
2025-04-09 20:36:20 -0500

    updating radio motor code

--- attiny_rx_motors/src/main.cpp
+++ attiny_rx_motors/src/main.cpp
@@ -2,7 +2,9 @@
 #include <RH_ASK.h>
 #include "radio_packet.h"
 
+void init_timer();
 void halt_error();
+void handle_position(radio_packet packet);
 
 // led defintions
 #define PIN_LED      PB0
@@ -21,6 +23,12 @@ void halt_error();
 
 RH_ASK radio(BAUD, PIN_RX);
 
+uint8_t position_enable;
+int8_t position_target;
+int8_t position_current;
+
+volatile uint8_t timer_position = 0;
+
 void setup() {
 
   pinMode(PIN_LED, OUTPUT);
@@ -30,6 +38,10 @@ void setup() {
   pinMode(PIN_MOTOR_B, OUTPUT);
   pinMode(PIN_FAULT, INPUT);
   
+  init_timer();
+
+  position_enable = 0;
+
   if (!radio.init()) {
     halt_error();
   }
@@ -38,8 +50,24 @@ void setup() {
 void loop() {
 
   if (radio.available()) {
+    uint8_t buffer_size = sizeof(radio_packet);
+    uint8_t buffer[buffer_size];
+    
+    if (radio.recv(buffer, &buffer_size)) {
+      radio_packet packet;
+      memcpy(&packet, buffer, buffer_size);
 
-  }  
+      handle_position(packet);
+    }
+  }
+
+  if (timer_position) {
+    update_position();
+  }
+}
+
+void init_timer() {
+  TCCR0A = 0;
 }
 
 // loop forever blinking the diagnostic LED quickly
@@ -51,4 +79,41 @@ void halt_error() {
     digitalWrite(PIN_LED, HIGH);
     delay(100);
   }
+}
+
+// update the position values if this is ours
+void handle_position(radio_packet packet) {
+  
+  // fake the "group" match
+  if (packet.group_id) {
+    position_target = packet.position;
+    position_enable = (packet.position == 0);
+  }
+}
+
+void update_position() {
+
+  if (! position_enable)
+    return;
+
+  position_current = map(
+    analogRead(PIN_SENSOR),
+    0, 1024,
+    -127, 127
+  );
+
+  if (position_current == 0)
+    position_current = 1;
+
+  if(position_current < position_target) {
+    digitalWrite(PIN_MOTOR_A, HIGH);
+    digitalWrite(PIN_MOTOR_B, LOW);
+  } else {
+    digitalWrite(PIN_MOTOR_A, LOW);
+    digitalWrite(PIN_MOTOR_B, HIGH);
+  }
+}
+
+ISR(TIMER0_OVF) {
+  timer_position = 1;
 }
